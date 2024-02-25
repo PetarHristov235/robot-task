b0d3062eeac916a1ed91a8fc67bf748831472429
PeturHristov <petarhristov235@abv.bg>
2024-02-25 02:41:56 +0200

    task ready, have to check it again

--- src/main/java/com/comsystem/homework/rest/RobotRestController.java
+++ src/main/java/com/comsystem/homework/rest/RobotRestController.java
@@ -1,26 +1,28 @@
 package com.comsystem.homework.rest;
 
 import com.comsystem.homework.model.RobotPlan;
-import org.springframework.http.HttpStatus;
+import com.comsystem.homework.robot.RobotOperations;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.comsystem.homework.robot.RobotOperations;
 
 @RestController()
 @RequestMapping("/api/v1/robot/operation")
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public final class RobotRestController {
+    RobotOperations robotOperations;
 
     /**
      * This method exposes the functionality of {@link RobotOperations#excavateStonesForDays(int)} via HTTP
      */
     @PostMapping("/excavation")
     public ResponseEntity<RobotPlan> excavateStones(@RequestParam Integer numberOfDays) {
-        // TODO
-        throw new ErrorResponseException(HttpStatus.I_AM_A_TEAPOT);
+        return ResponseEntity.ok(robotOperations.excavateStonesForDays(numberOfDays));
     }
 
     /**
@@ -28,8 +30,6 @@ public final class RobotRestController {
      */
     @PostMapping("/approximation")
     public ResponseEntity<RobotPlan> approximateDays(@RequestParam Integer numberOfStones) {
-        // TODO
-        throw new ErrorResponseException(HttpStatus.I_AM_A_TEAPOT);
+        return ResponseEntity.ok(robotOperations.daysRequiredToCollectStones(numberOfStones));
     }
-
 }
